4255a43421da74a28c8a39a8c57a7789d8ad2031
Daniil Efimow <daniil.efimow@mail.ru>
2025-05-19 19:00:31 +0300

    'new_list'

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def print_list():
-    list = [1, 2, 3, 4, 5]
+def print_list(num):
+    list = []
+    for i in range(1, num + 1):
+        list.append(i)
     print(list)
 
-print_list()
+n = int(input())
+
+print_list(n)
